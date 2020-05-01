ec1b39936bc517e219cdfbf72516141c329aa157
FranLopezP <francisco-9708@hotmail.es>
2020-04-30 20:36:12 -0500

    Nueva vista

---
+++ src/public/precache-manifest.5ff6925a40dcc1d596632baf1f07b1a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86aa37b2e1e207d82ebe37e6f8da64b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feda424f1dea438ab9c9",
+    "url": "/static/css/2.5b3da1b9.chunk.css"
+  },
+  {
+    "revision": "38cefa167d5fa85685f8",
+    "url": "/static/css/main.7ed84b26.chunk.css"
+  },
+  {
+    "revision": "feda424f1dea438ab9c9",
+    "url": "/static/js/2.07404d1f.chunk.js"
+  },
+  {
+    "revision": "6c99299aa0ccf7cd363681b2638ef8ef",
+    "url": "/static/js/2.07404d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cefa167d5fa85685f8",
+    "url": "/static/js/main.cab4b120.chunk.js"
+  },
+  {
+    "revision": "fdf93752cd2a077f8069",
+    "url": "/static/js/runtime-main.30fad242.js"
+  }
+]);
--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de91ce66792dbac9f85a6a20374b1b0f.js"
+  "/precache-manifest.5ff6925a40dcc1d596632baf1f07b1a5.js"
 );
 
 self.addEventListener('message', (event) => {
